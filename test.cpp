9e6258cfef2a9dc20ef4146f40cf1b65de8c3d9c
Muhammad Alfian <pianaee379@gmail.com>
2021-05-09 08:47:50 +0700

    menambahkan cin dan cout cin

--- test.cpp
+++ test.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main(){
-
+    int a;
     cout << "Hello, world!" << endl;
     cout << "This is my code!" << endl;
+    cin >> a;
+    cout << a << endl;
 
     cin.get();
     return 0;
